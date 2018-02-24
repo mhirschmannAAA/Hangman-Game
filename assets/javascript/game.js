b6f7f8e18e8ee8404b7f79421771b91dea064e26
mhirschmann <mhirschmann@national.aaa.com>
2018-02-23 20:14:48 -0500

    post tutor help

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,87 @@
-window.onload = function() {
+window.onload = function () {
+  words = ["cannondale", "trek", "giant", "litespeed"]
   var word;
   var guess;
   var guesses = [];
   var lives;
   var counter;
-  var gameState  = "start";
+  var gameState = "start";
 
   // Get elements
   var showLives = document.getElementById("mylives");
   var showCatagory = document.getElementById("scatagory");
 
-  // Create geusses ul
-  result = function () {
+  // Create guesses ul
+  var result = function () {
     wordHolder = document.getElementById('hold');
     correct = document.createElement('ul');
+    correct.setAttribute('id', 'my-word');
 
     for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word');
+
       guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
+      guess.setAttribute('class', i);
       if (word[i] === "-") {
         guess.innerHTML = "-";
         space = 1;
       } else {
-        guess.innerHTML = "_";
+        guess.innerHTML = "_  ";
       }
 
-      geusses.push(guess);
+      guesses.push(guess);
       wordHolder.appendChild(correct);
       correct.appendChild(guess);
     }
   }
 
-    // Show lives
-    comments = function () {
-      showLives.innerHTML = "You have " + lives + " lives";
-      if (lives < 1) {
-        showLives.innerHTML = "Game Over";
-      }
-      for (var i = 0; i < geusses.length; i++) {
-        if (counter + space === geusses.length) {
-          showLives.innerHTML = "You Win!";
-        }
-      }
+  // Show lives
+  comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      showLives.innerHTML = "Game Over";
     }
-
-    // OnClick Function
-   check = function () {
-     list.onclick = function () {
-      var geuss = (this.innerHTML);
-      this.setAttribute("class", "active");
-      this.onkeyup = null;
-      for (var i = 0; i < word.length; i++) {
-        if (word[i] === geuss) {
-          geusses[i].innerHTML = geuss;
-          counter += 1;
-        } 
-      }
-      var j = (word.indexOf(geuss));
-      if (j === -1) {
-        lives -= 1;
-        comments();
-        animate();
-      } else {
-        comments();
+    for (var i = 0; i < guesses.length; i++) {
+      if (counter + space === guesses.length) {
+        showLives.innerHTML = "You Win!";
       }
     }
   }
 
-  play = function() {
-    words = ["cannondale","trek","giant","litespeed"]
-    word = chosenWord[Math.floor(Math.random() * chosenWord.length)];
+  // OnClick Function
+  var check = function (value) {
+    console.log(value)
+    for (var i = 0; i < word.length; i++) {
+      if (value === word[i]) {
+        document.getElementsByClassName(i)[0].innerHTML = value;
+      }
+
+    }
+      // var guess = (this.innerHTML);
+      // this.setAttribute("class", "active");
+      // this.onkeyup = null;
+      // for (var i = 0; i < word.length; i++) {
+      //   if (word[i] === guess) {
+      //     guesses[i].innerHTML = guess;
+      //     counter += 1;
+      //   }
+      // }
+      // var j = (word.indexOf(guess));
+      // if (j === -1) {
+      //   lives -= 1;
+      //   comments();
+      //   animate();
+      // } else {
+      //   comments();
+      // }
+    
+  }
+
+  var play = function () {
+
+    word = words[Math.floor(Math.random() * words.length)];
     word = word.replace(/\s/g, "-");
     console.log(word);
-    buttuns();
+    // buttons();
 
     guesses = [];
     lives = 10;
@@ -81,19 +89,25 @@ window.onload = function() {
     result();
   }
 
-  play();
-
-  document.getElementById('reset').onclick = function() {
+  document.getElementById('reset').onclick = function () {
     correct.parentNode.removeChild(correct);
     letters.parentNode.removeChild(letters);
     showClue.innerHTML = "";
     context.clearRect(0, 0, 400, 400);
     play();
   }
-}
 
-document.onkeyup = function(event) {
-  if (userInput === "k")
-    play();
-  console.log(gameState)
+  document.onkeyup = function (event) {
+    
+    if (event.key === " ") {
+      play();
+    }
+
+    if (/[a-z]/gi.test(event.key)) {
+      
+      check(event.key);
+    }
+
+    console.log(gameState)
+  }
 }
